775d694eb21e6fbcd77c97ad1e94be334e0e2328
shafed <shaparenko.fedor@gmail.com>
2026-02-28 07:24:27 +0300

    decimal folders

--- python/10.py
+++
@@ -1,4 +0,0 @@
-d = {1: "Один", 2: "Два", 3: "Три"}
-print(sum(d.keys()))
-print(max(d), min(d))
-print(len(d))
--- python/11.py
+++
@@ -1,2 +0,0 @@
-d = {"pi": 3.14, "e": 2.71, "fi": 1.62}
-print(*list(i for i in d.values() if i > 2.5))
