92b29c888aa9c9b490222139fe60d4fb3121fb47
shafed <shaparenko.fedor@gmail.com>
2026-03-12 18:23:06 +0300

    python

--- 01-python/codewars.py
+++ 01-python/codewars.py
@@ -1,7 +1,3 @@
-def find_nb(m):
-    ttl = 0
-    n = 1
-    while ttl < m:
-        ttl += n**3
-        n += 1
-    return n - 1 if ttl == m else -1
+def duplicate_encode(word):
+    word = word.lower()
+    return "".join(")" if word.count(i) > 1 else "(" for i in word)
