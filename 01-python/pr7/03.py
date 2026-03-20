b3b30c45fd3b48c224f4841e86deb930c0f4b391
shafed <shaparenko.fedor@gmail.com>
2026-03-20 13:37:05 +0000

    one more

--- 01-python/pr7/03.py
+++ 01-python/pr7/03.py
@@ -16,13 +16,29 @@ class Family:
 
     def earn_money(self, plus):
         self.money += plus
-        print(f"Earned {plus} money!")
+        print(
+            f"Earned {plus} money!",
+        )
         print("Current value:", self.money)
 
     def buy_house(self, price, discount=0):
-        if self.money >= price * (1 - discount):
+        price_with_discount = price * (1 - discount)
+        if self.money >= price_with_discount:
+            self.money -= price_with_discount
             print("House purchased!")
             print("Current money:", self.money)
         else:
             print("Not enough money!")
             print("Try to buy a house again")
+
+
+family = Family("Common", 100_000, False)
+family.info()
+print()
+family.buy_house(900_000)
+print()
+family.earn_money(800_000)
+print()
+family.buy_house(900_000)
+print()
+family.info()
